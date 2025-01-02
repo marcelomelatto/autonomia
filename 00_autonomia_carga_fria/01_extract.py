949c58c5903ed2857c4dbb444c2125bb44e9c374
marcelomelatto <marcelomelatto@gmail.com>
2025-01-02 17:41:58 +0000

    mais cargas frias

--- 00_autonomia_carga_fria/01_extract.py
+++ 00_autonomia_carga_fria/01_extract.py
@@ -13,10 +13,21 @@ BASE_DIR = os.getenv(
     "BASE_DIR",
     "/mnt/shared/ANALYTICS_LOGISTICAS/00.Bases de Dados/7.SHAREPOINT/Cadastros",
 )
-LOCAL_ATLAS_PATH = os.path.join(BASE_DIR, "DIMENSAO_LOCAL_ATLAS.xlsx")
-LOCAL_SAP_PATH = "/home/cdsw/autonomia/notebooks/DIMENSAO_LOCAL_SAP.xlsx"
-COMPATIBILIDADE_PATH = os.path.join(BASE_DIR, "CADASTRO_COMPATIBILIDADE_FAMILIA.xlsx")
-MATERIAL_PATH = os.path.join(BASE_DIR, "DIMENSAO_MATERIAL.xlsx")
+
+BASE_CDSW = os.getenv("BASE_CDSW", "/home/cdsw/autonomia/notebooks/")
+
+# Paths para os arquivos
+DIM_CALENDARIZACAO_PATH = os.path.join(BASE_DIR, "CADASTRO_CALENDARIZACAO.xlsx")
+DIM_CENTRO_DEPOSITO_PATH = os.path.join(BASE_DIR, "DIM_CENTRO_DEPOSITO.xlsx")
+DIM_CENTROS_MANTER_PATH = os.path.join(BASE_CDSW, "CENTROS_MANTER_v20-12.xlsx")
+DIM_COMPATIBILIDADE_PATH = os.path.join(BASE_DIR, "CADASTRO_COMPATIBILIDADE_FAMILIA.xlsx")
+DIM_DEPOSITOS_EXCLUIR_PATH = os.path.join(BASE_CDSW, "CENTROS_MANTER_v20-12.xlsx")
+DIM_FAMILIA_PATH = os.path.join(BASE_DIR, "DIM_FAMILIA.xlsx")
+DIM_LOCAL_ATLAS_PATH = os.path.join(BASE_DIR, "DIM_LOCAL_ATLAS.xlsx")
+DIM_LOCAL_SAP_PATH = os.path.join(BASE_CDSW, "DIM_LOCAL_SAP.xlsx")
+DIM_MATERIAL_PATH = os.path.join(BASE_DIR, "DIM_MATERIAL.xlsx")
+DIM_RESPONSAVEL_PATH = os.path.join(BASE_DIR, "DIM_RESPONSAVEL.xlsx")
+DIM_SAZONALIDADE_PATH = os.path.join(BASE_DIR, "sazonalidade_decomposta.xlsx")
 
 
 # Função genérica para ler arquivos Excel
@@ -41,11 +52,11 @@ def load_excel(file_path, sheet_name=None, dtype=None):
 
 # Carregar DataFrames
 
-########################
-# dimensao_local_atlas #
-########################
+###################
+# dim_local_atlas #
+###################
 df_dim_local_atlas = load_excel(
-    file_path=LOCAL_ATLAS_PATH,
+    file_path=DIM_LOCAL_ATLAS_PATH,
     sheet_name="in",
     dtype={"ID_LOCAL": np.int32, "COD_LOCAL_SAP": np.str_},
 )
@@ -57,7 +68,7 @@ df_dim_local_atlas.columns = df_dim_local_atlas.columns.str.lower()
 # cadastro_compatibilidade_familia #
 ####################################
 df_compatibilidade = load_excel(
-    file_path=COMPATIBILIDADE_PATH,
+    file_path=DIM_COMPATIBILIDADE_PATH,
     dtype={
         "TIPO_LOCAL": np.str_,
         "TIPO_ATENDIMENTO": np.str_,
@@ -74,11 +85,11 @@ df_compatibilidade = load_excel(
 # Padronizar colunas para minúsculas
 df_compatibilidade.columns = df_compatibilidade.columns.str.lower()
 
-#####################
-# dimensao_material #
-#####################
+################
+# dim_material #
+################
 df_dim_material = load_excel(
-    file_path=MATERIAL_PATH,
+    file_path=DIM_MATERIAL_PATH,
     sheet_name="in",
     dtype={
         "NUM_MATERIAL": np.str_,
@@ -91,12 +102,79 @@ df_dim_material = load_excel(
 # Padronizar colunas para minúsculas
 df_dim_material.columns = df_dim_material.columns.str.lower()
 
-######################
-# dimensao_local_sap #
-######################
+#################
+# dim_local_sap #
+#################
 dimensao_local_sap = load_excel(
-    file_path=LOCAL_SAP_PATH, sheet_name="in", engine="openpyxl"
+    file_path=DIM_LOCAL_SAP_PATH, sheet_name="in", engine="openpyxl"
 )
 
 # Padronizar colunas para minúsculas
 dimensao_local_sap.columns = dimensao_local_sap.columns.str.lower()
+
+#####################
+# depositos_excluir #
+#####################
+df_depositos_excluir = load_excel(
+    file_path=DIM_DEPOSITOS_EXCLUIR_PATH,
+    sheet_name="DEPOSITO_EXCLUIR",
+    engine="openpyxl",
+)
+
+# Padronizar colunas para minúsculas
+df_depositos_excluir.columns = df_depositos_excluir.columns.str.lower()
+
+##################
+# centros_manter #
+##################
+df_centros_manter = load_excel(file_path=DIM_CENTROS_MANTER_PATH, engine="openpyxl")
+
+# Padronizar colunas para minúsculas
+df_centros_manter.columns = df_centros_manter.columns.str.lower()
+
+###############
+# dim_familia #
+###############
+df_dim_familia = load_excel(
+    file_path=DIM_FAMILIA_PATH,
+    sheet_name="in",
+    dtype={"DSC_FAMILIA": np.str_, "STATUS_FAMILIA": np.str_},
+)
+
+# Padronizar colunas para minúsculas
+df_dim_familia.columns = df_dim_familia.columns.str.lower()
+
+#######################
+# dim_centro_deposito #
+#######################
+df_dim_centro_deposito = load_excel(file_path=DIM_CENTRO_DEPOSITO_PATH, sheet_name="in")
+
+# Padronizar colunas para minúsculas
+df_dim_centro_deposito.columns = df_dim_centro_deposito.columns.str.lower()
+
+###################
+# dim_responsavel #
+###################
+df_dim_responsavel = load_excel(
+    file_path=DIM_RESPONSAVEL_PATH,
+    sheet_name="IN",
+    dtype={
+        "UF": np.str_,
+        "RESPONSAVEL": np.str_,
+        "TIPO_MATERIAL": np.str_,
+        "qtde_multiplo_envio": np.datetime64,
+        "qtde_multiplo_consumo": np.datetime64,
+    },
+)
+
+# Padronizar colunas para minúsculas
+df_dim_responsavel.columns = df_dim_responsavel.columns.str.lower()
+
+
+##################
+# calendarizacao #
+##################
+df_calendarizacao = load_excel(file_path=DIM_CALENDARIZACAO_PATH, sheet_name="in")
+
+# Padronizar colunas para minúsculas
+df_calendarizacao.columns = df_calendarizacao.columns.str.lower()
